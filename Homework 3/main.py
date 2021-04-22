a867b04c1c33196ee98d6c1e34143579ff568261
Matthew M <mattemoye@gmail.com>
2021-04-22 02:08:15 -0400

    Add option to change sample size in main.py

--- Homework 3/main.py
+++ Homework 3/main.py
@@ -11,19 +11,35 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.backends.cudnn.benchmark = True
 torch.manual_seed(1)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', choices=["DCGAN", "WGAN", "ACGAN"], required=True, help="Different Model Choices")
+parser.add_argument('--evaluate', choices=["train", "generate"], required=True, help="Whether to train a model or generate images")
+parser.add_argument('--sample_size', default=2560, help="Number of images to generate and to sample from training set")
+args = parser.parse_args()
+print(args)
+
+if args.model == "DCGAN":
+    from model_DCGAN import Generator, train
+elif args.model == "WGAN":
+    from model_WGAN import Generator, train
+elif args.model == "ACGAN":
+    from model_ACGAN import Generator, train
+else:
+    print("Only DCGAN, WGAN, and ACGAN models can be ran")
+    exit()
+
 # Generate images, and save images
 def generate_results(model):
-    num_samples = 2560
     if model == "DCGAN":
-        from model_DCGAN import Generator
-        fixed_noise = torch.randn(num_samples, 100, 1, 1, device=device)
+        fixed_noise = torch.randn(args.sample_size, 100, 1, 1, device=device)
         model_file = "netG_epoch_100.pth"
     elif model == "WGAN":
-        from model_WGAN import Generator
-        fixed_noise = torch.randn(num_samples, 100, device=device)
+        fixed_noise = torch.randn(args.sample_size, 100, device=device)
         model_file = "netG_epoch_400.pth"
     else:
-        from model_ACGAN import Generator
+        fixed_noise = torch.randn(args.sample_size, 100, 1, 1, device=device)
+        model_file = "netG_epoch_100.pth"
+
     if not os.path.exists("results/{}/images_fake".format(model)):
         os.makedirs("results/{}/images_fake".format(model))
         os.makedirs("results/{}/images_real".format(model))
@@ -37,34 +53,20 @@ def generate_results(model):
     netG = Generator().to(device)
     netG.load_state_dict(torch.load("results/{}/models/{}".format(model,model_file)))
     netG.eval()
+
     # Generate images
     with torch.no_grad():
         fake_imgs = netG(fixed_noise)
+
     # Save images from CIFAR-10 and generated images to directory
     for i, data in enumerate(dataloader):
-        if i == num_samples:
+        if i == args.sample_size:
             break
         X = data[0].to(device)
         save_image(X,'results/{}/images_real/real_sample_{}.png'.format(model,i+1),normalize=True)
         save_image(fake_imgs[i].detach(),'results/{}/images_fake/fake_sample_{}.png'.format(model,i+1),normalize=True)
 
-# Train model
-def train(model):
-    if model == "DCGAN":
-        from model_DCGAN import train
-    elif model == "WGAN":
-        from model_WGAN import train
-    else:
-        from model_ACGAN import train
-    train()
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', choices=["DCGAN", "WGAN", "ACGAN"], required=True, help="Different Model Choices")
-parser.add_argument('--evaluate', choices=["train", "generate"], required=True, help="Whether to train a model or generate images")
-args = parser.parse_args()
-print(args)
-
 if args.evaluate == "train":
-    train(args.model)
+    train()
 else:
     generate_results(args.model)
